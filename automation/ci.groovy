8b232e6ef3563e372525cee4d88f3418a72d2a9e
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-05-19 17:45:56 -0500

    Adding pipeline

--- automation/ci.groovy
+++ automation/ci.groovy
@@ -5,7 +5,7 @@ node {
     stage("Test") {
 
         docker.image('maven:3.3.9-jdk-8-alpine')
-                .withRun('-v $HOME/.m2:/root/.m2') {
+                .inside('-v $HOME/.m2:/root/.m2') {
 
             sh 'mvn -v'
             sh "mvn clean test"
