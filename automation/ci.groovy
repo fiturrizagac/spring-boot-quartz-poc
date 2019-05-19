a1d6c207239a4b0275debc85a27d0b6fcf931571
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-05-19 17:11:22 -0500

    Adding pipeline

--- automation/ci.groovy
+++ automation/ci.groovy
@@ -1,10 +1,15 @@
 node {
 
+    agent {
+        docker {
+            image 'maven:3.3.9-jdk-8-alpine'
+        }
+    }
+
     stage("Test") {
         checkout scm
+        sh 'mvn -v'
         sh "mvn clean test"
-        // sonar
-        // fortify
     }
 
 }
