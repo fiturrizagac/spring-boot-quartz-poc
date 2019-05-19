2666976c8568563eea3e5620c9267e61cf340186
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-05-19 17:22:13 -0500

    Adding pipeline

--- automation/ci.groovy
+++ automation/ci.groovy
@@ -5,16 +5,14 @@ pipeline {
             image 'maven:3.3.9-jdk-8-alpine'
             args '-v $HOME/.m2:/root/.m2'
         }
-    }
 
-    node {
-        stage("Test") {
-            checkout scm
-            sh 'mvn -v'
-            sh "mvn clean test"
+        node {
+            stage("Test") {
+                checkout scm
+                sh 'mvn -v'
+                sh "mvn clean test"
+            }
         }
     }
-
-
 
 }
