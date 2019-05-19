150c5073795f691d2d70a078c7aa968eafe3b769
Franco Iturrizaga <fiturrizagac@gmail.com>
2019-05-19 17:20:40 -0500

    Adding pipeline

--- automation/ci.groovy
+++ automation/ci.groovy
@@ -1,15 +1,20 @@
-node {
+pipeline {
 
     agent {
         docker {
             image 'maven:3.3.9-jdk-8-alpine'
+            args '-v $HOME/.m2:/root/.m2'
         }
     }
 
-    stage("Test") {
-        checkout scm
-        sh 'mvn -v'
-        sh "mvn clean test"
+    node {
+        stage("Test") {
+            checkout scm
+            sh 'mvn -v'
+            sh "mvn clean test"
+        }
     }
+
+
 
 }
